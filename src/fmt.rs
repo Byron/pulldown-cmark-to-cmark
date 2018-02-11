f315ab75743e40607acc1010514cad4d8b1eabc3
Sebastian Thiel <byronimo@gmail.com>
2018-02-11 10:20:19 +0100

    First bunch of API docs

--- src/fmt.rs
+++ src/fmt.rs
@@ -3,7 +3,11 @@ use std::borrow::Borrow;
 use std::borrow::Cow;
 use pulldown_cmark::{Alignment as TableAlignment, Event};
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+/// Similar to [Pulldown-Cmark-Alignment][pd-alignment], but with required
+/// traits for comparison to allow testing.
+///
+/// [pd-alignment]: https://docs.rs/pulldown-cmark/*/pulldown_cmark/enum.Alignment.html
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum Alignment {
     None,
     Left,
@@ -22,16 +26,31 @@ impl<'a> From<&'a TableAlignment> for Alignment {
     }
 }
 
-#[derive(Default, Clone, Debug, PartialEq, Eq)]
+/// The state of the `cmark` function.
+/// This does not only allow introspection, but enables the user
+/// to halt the serialization at any time, and resume it later.
+#[derive(Clone, Default, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct State<'a> {
+    /// The amount of newlines to insert after `Event::Start(...)`
     pub newlines_before_start: usize,
+    /// The lists and their types for which we have seen a `Event::Start(List(...))` tag
     pub list_stack: Vec<Option<usize>>,
+    /// The computed padding and prefix to print after each newline.
+    /// This changes with the level of `BlockQuote` and `List` events.
     pub padding: Vec<Cow<'a, str>>,
+    /// Keeps the current table alignments, if we are currently serializing a table.
     pub table_alignments: Vec<Alignment>,
+    /// Keeps the current table headers, if we are currently serializing a table.
     pub table_headers: Vec<String>,
 }
 
-#[derive(Clone, Debug)]
+/// Configuration for the `cmark` function.
+/// The defaults should provide decent spacing and most importantly, will
+/// provide a faithful rendering of your markdown document particularly when
+/// rendering it to HTML.
+///
+/// It's best used with its `Options::default()` implementation.
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Options {
     pub newlines_after_headline: usize,
     pub newlines_after_paragraph: usize,
@@ -58,9 +77,27 @@ impl Default for Options {
     }
 }
 
+/// Serialize a stream of [pulldown-cmark-Events][pd-event] into a string-backed buffer.
+///
+/// 1. **events**
+///   * An iterator over [`Events`][pd-event], for example as returned by the [`Parser`][pd-parser]
+/// 1. **formatter**
+///   * A format writer, can be a `String`.
+/// 1. **state**
+///   * The optional initial state of the serialization.
+/// 1. **options**
+///   * Customize the appearance of the serialization. All otherwise magic values are contained
+///     here.
+///
+/// *Returns* the `State` of the serialization on success. You can use it as initial state in the
+/// next call if you are halting event serialization.
+/// *Errors* are only happening if the underlying buffer fails, which is unlikely.
+///
+/// [pd-event]: https://docs.rs/pulldown-cmark/*/pulldown_cmark/enum.Event.html
+/// [pd-parser]: https://docs.rs/pulldown-cmark/*/pulldown_cmark/struct.Parser.html
 pub fn cmark_with_options<'a, I, E, F>(
     events: I,
-    mut f: F,
+    mut formatter: F,
     state: Option<State<'static>>,
     options: Options,
 ) -> Result<State<'static>, fmt::Error>
@@ -139,14 +176,14 @@ where
                     _ => {}
                 }
                 let left_on_padded_newlines = state.newlines_before_start != 0;
-                consume_newlines(&mut f, &mut state)?;
+                consume_newlines(&mut formatter, &mut state)?;
                 match *tag {
                     Item => match state.list_stack.last() {
                         Some(inner) => {
                             state.padding.push(padding_of(*inner));
                             match inner {
-                                &Some(n) => write!(f, "{}. ", n),
-                                &None => f.write_str("* "),
+                                &Some(n) => write!(formatter, "{}. ", n),
+                                &None => formatter.write_str("* "),
                             }
                         }
                         None => Ok(()),
@@ -157,44 +194,44 @@ where
                     }
                     TableHead => Ok(()),
                     TableRow => Ok(()),
-                    TableCell => f.write_char('|'),
-                    Link(_, _) => f.write_char('['),
-                    Image(_, _) => f.write_str("!["),
-                    Emphasis => f.write_char('*'),
-                    Strong => f.write_str("**"),
-                    Code => f.write_char('`'),
-                    FootnoteDefinition(ref name) => write!(f, "[^{}]: ", name),
+                    TableCell => formatter.write_char('|'),
+                    Link(_, _) => formatter.write_char('['),
+                    Image(_, _) => formatter.write_str("!["),
+                    Emphasis => formatter.write_char('*'),
+                    Strong => formatter.write_str("**"),
+                    Code => formatter.write_char('`'),
+                    FootnoteDefinition(ref name) => write!(formatter, "[^{}]: ", name),
                     Paragraph => Ok(()),
-                    Rule => f.write_str("---"),
+                    Rule => formatter.write_str("---"),
                     Header(n) => {
                         for _ in 0..n {
-                            f.write_char('#')?;
+                            formatter.write_char('#')?;
                         }
-                        f.write_char(' ')
+                        formatter.write_char(' ')
                     }
                     BlockQuote => if !left_on_padded_newlines {
-                        padding(&mut f, &state.padding)
+                        padding(&mut formatter, &state.padding)
                     } else {
                         Ok(())
                     },
-                    CodeBlock(ref info) => f.write_str("```")
-                        .and(f.write_str(info))
-                        .and(f.write_char('\n'))
-                        .and(padding(&mut f, &state.padding)),
+                    CodeBlock(ref info) => formatter.write_str("```")
+                        .and(formatter.write_str(info))
+                        .and(formatter.write_char('\n'))
+                        .and(padding(&mut formatter, &state.padding)),
                     List(_) => Ok(()),
                 }
             }
             End(ref tag) => match *tag {
                 Image(ref uri, ref title) | Link(ref uri, ref title) => {
                     if title.is_empty() {
-                        write!(f, "]({})", uri)
+                        write!(formatter, "]({})", uri)
                     } else {
-                        write!(f, "]({uri} \"{title}\")", uri = uri, title = title)
+                        write!(formatter, "]({uri} \"{title}\")", uri = uri, title = title)
                     }
                 }
-                Code => f.write_char('`'),
-                Emphasis => f.write_char('*'),
-                Strong => f.write_str("**"),
+                Code => formatter.write_char('`'),
+                Emphasis => formatter.write_char('*'),
+                Strong => formatter.write_str("**"),
                 Header(_) => {
                     if state.newlines_before_start < options.newlines_after_headline {
                         state.newlines_before_start = options.newlines_after_headline;
@@ -211,7 +248,7 @@ where
                     if state.newlines_before_start < options.newlines_after_codeblock {
                         state.newlines_before_start = options.newlines_after_codeblock;
                     }
-                    f.write_str("```")
+                    formatter.write_str("```")
                 }
                 Rule => {
                     if state.newlines_before_start < options.newlines_after_rule {
@@ -232,19 +269,19 @@ where
                     if state.newlines_before_start < options.newlines_after_rest {
                         state.newlines_before_start = options.newlines_after_rest;
                     }
-                    f.write_char('|')?;
+                    formatter.write_char('|')?;
 
                     if let &TableHead = t {
-                        f.write_char('\n').and(padding(&mut f, &state.padding))?;
+                        formatter.write_char('\n').and(padding(&mut formatter, &state.padding))?;
                         for (alignment, name) in state
                             .table_alignments
                             .iter()
                             .zip(state.table_headers.iter())
                         {
-                            f.write_char('|')?;
+                            formatter.write_char('|')?;
                             let last_minus_one = name.len() - 1;
                             for c in 0..name.len() {
-                                f.write_char(if (c == 0
+                                formatter.write_char(if (c == 0
                                     && (alignment == &Alignment::Center
                                         || alignment == &Alignment::Left))
                                     || (c == last_minus_one
@@ -257,7 +294,7 @@ where
                                 })?;
                             }
                         }
-                        f.write_char('|')?;
+                        formatter.write_char('|')?;
                     }
                     Ok(())
                 }
@@ -283,34 +320,35 @@ where
                 }
                 FootnoteDefinition(_) => Ok(()),
             },
-            HardBreak => f.write_str("  \n").and(padding(&mut f, &state.padding)),
-            SoftBreak => f.write_char('\n').and(padding(&mut f, &state.padding)),
+            HardBreak => formatter.write_str("  \n").and(padding(&mut formatter, &state.padding)),
+            SoftBreak => formatter.write_char('\n').and(padding(&mut formatter, &state.padding)),
             Text(ref text) => {
                 if state.table_alignments.len() != state.table_headers.len() {
                     state.table_headers.push(text.clone().into());
                 }
-                consume_newlines(&mut f, &mut state)?;
-                print_text(text, &mut f, &state.padding)
+                consume_newlines(&mut formatter, &mut state)?;
+                print_text(text, &mut formatter, &state.padding)
             }
             Html(ref text) => {
-                consume_newlines(&mut f, &mut state)?;
-                print_text(text, &mut f, &state.padding)?;
+                consume_newlines(&mut formatter, &mut state)?;
+                print_text(text, &mut formatter, &state.padding)?;
 
                 if state.newlines_before_start < options.newlines_after_html {
                     state.newlines_before_start = options.newlines_after_html;
                 }
                 Ok(())
             }
-            InlineHtml(ref name) => f.write_str(name),
-            FootnoteReference(ref name) => write!(f, "[^{}]", name),
+            InlineHtml(ref name) => formatter.write_str(name),
+            FootnoteReference(ref name) => write!(formatter, "[^{}]", name),
         }?
     }
     Ok(state)
 }
 
+/// As `cmark_with_options`, but with default `Options`.
 pub fn cmark<'a, I, E, F>(
     events: I,
-    f: F,
+    formatter: F,
     state: Option<State<'static>>,
 ) -> Result<State<'static>, fmt::Error>
 where
@@ -318,5 +356,5 @@ where
     E: Borrow<Event<'a>>,
     F: fmt::Write,
 {
-    cmark_with_options(events, f, state, Options::default())
+    cmark_with_options(events, formatter, state, Options::default())
 }
